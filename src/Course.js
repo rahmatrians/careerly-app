39446ded61fa3e3dbbd4f9736dd20232cc585acf
afifavinaa <afifa.vina1@gmail.com>
2022-04-16 23:45:49 +0700

    update

--- src/Course.js
+++ src/Course.js
@@ -9,13 +9,13 @@ function Course() {
   let nav = useNavigate();
   const [search, setSearch] = useState("");
   const [getData, setGetData] = useState([]);
-  
+
   useEffect(() => {
   }, [getData])
 
 
   const detailPage = async (data) => {
-console.log('detail cui',data);
+    console.log('detail cui', data);
     // fetch("http://localhost:8000/detail", {
     //   method: "POST",
     //   headers: {'Content-Type': 'application/json'}, 
@@ -27,13 +27,13 @@ console.log('detail cui',data);
     await axios.post('http://localhost:8000/detail', {
       data: data
     })
-    .then((response) => {
-      console.log(response.data);
-      nav({pathname:`/detail`, state:data});
-    }, (error) => {
-      console.log(error);
-    });
-    
+      .then((response) => {
+        console.log(response.data);
+        nav({ pathname: `/detail`, state: data });
+      }, (error) => {
+        console.log(error);
+      });
+
   }
 
 
@@ -49,7 +49,7 @@ console.log('detail cui',data);
         setGetData(html);
       })
   }
-  
+
   //  let dataKu = [
   //     {nama : 'react js aja', harga : 'Rp. 400.000'},
   //     {nama : 'flutter nih', harga : 'Rp. 220.000'},
@@ -61,7 +61,7 @@ console.log('detail cui',data);
 
 
 
- return(
+  return (
     <>
       <section className="fixed z-50 top-0 left-0 right-0">
         <div className="navbar bg-white drop-shadow-[0_35px_35px_rgba(168,170,225,0.07)]">
@@ -83,46 +83,46 @@ console.log('detail cui',data);
 
       <section>
         <div className=" mt-40">
-        <div className="form-control bg-white self-center w-3/6 mx-auto drop-shadow-[0_35px_35px_rgba(168,170,225,0.15)] p-4 h-4/6 rounded-lg">
-          <div className="input-group">
-          <input value={search} onChange={e => setSearch(e.target.value)}  type="text" placeholder="Cari Kelas impianmu..." className="input input-bordered input-primary w-full"/>
-            <button onClick={() => searchingData()}  className="btn btn-square btn-primary">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-            </button>
+          <div className="form-control bg-white self-center w-3/6 mx-auto drop-shadow-[0_35px_35px_rgba(168,170,225,0.15)] p-4 h-4/6 rounded-lg">
+            <div className="input-group">
+              <input value={search} onChange={e => setSearch(e.target.value)} type="text" placeholder="Cari Kelas impianmu..." className="input input-bordered input-primary w-full" />
+              <button onClick={() => searchingData()} className="btn btn-square btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+              </button>
+            </div>
           </div>
-        </div>
         </div>
       </section>
 
 
       <section>
-      <div className="container mx-auto mt-32">
-            <div className="grid grid-cols-3 gap-x-12 gap-y-20 mx-28">
+        <div className="container mx-auto mt-32">
+          <div className="grid grid-cols-3 gap-x-12 gap-y-20 mx-28">
 
-  {getData.map((val, idx) => (
-  <div key={idx} className=" card bg-white drop-shadow-[0_35px_35px_rgba(168,170,225,0.15)] grid content-between">
-                    <div className="grid justify-items-start">
-                      <figure className="px-10 pt-10  mx-auto">
-                        <img src={val.logo} alt="Shoes" className="rounded-xl max-h-[150px]" />
-                      </figure>
-                    </div>
-                    <div className="card-body">
-                      <h2 className="text-xl font-bold text-[#3F427B]">{val.job}</h2>
-                      <p className="text-lg text-[#3F427B]">{val.location}</p>
-                      <div className="card-actions flex mt-12">
-                      <button className="btn btn-primary w-full self-end font-bold" onClick={() => detailPage(val.detail)}>Lihat</button>
-                      </div>
-                      </div>
-                    </div>
-                    ))}
-                
+            {getData.map((val, idx) => (
+              <div key={idx} className=" card bg-white drop-shadow-[0_35px_35px_rgba(168,170,225,0.15)] grid content-between">
+                <div className="grid justify-items-start">
+                  <figure className="px-10 pt-10  mx-auto">
+                    <img src={val.logo} alt="Shoes" className="rounded-xl max-h-[150px]" />
+                  </figure>
                 </div>
+                <div className="card-body">
+                  <h2 className="text-xl font-bold text-[#3F427B]">{val.job}</h2>
+                  <p className="text-lg text-[#3F427B]">{val.location}</p>
+                  <div className="card-actions flex mt-12">
+                    <button className="btn btn-primary w-full self-end font-bold" onClick={() => detailPage(val.detail)}>Lihat</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+
           </div>
+        </div>
 
       </section>
-     
+
     </>
- );
+  );
 }
 
 export default Course;
--- src/index.js
+++ src/index.js
@@ -6,23 +6,25 @@ import Home from './Home';
 import Login from './Login';
 import Course from './Course';
 import CourseDetail from './CourseDetail';
+import SettingUser from './SettingUser'
 import reportWebVitals from './reportWebVitals';
 
-import {   BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    {/* <Course /> */}
-    <Routes>
+    <BrowserRouter>
+      {/* <Course /> */}
+      <Routes>
         <Route index element={<Home />} />
         <Route path="login" element={<Login />}></Route>
         <Route path="course" element={<Course />} />
         <Route path="detail" element={<CourseDetail />} />
-          {/* <Route path=":teamId" element={<Team />} /> */}
-          {/* <Route index element={<LeagueStandings />} /> */}
-    </Routes>
-  </BrowserRouter>
+        <Route path="settinguser" element={<SettingUser />} />
+        {/* <Route path=":teamId" element={<Team />} /> */}
+        {/* <Route index element={<LeagueStandings />} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
